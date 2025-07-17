ddc7c1f70409670863b60a9dc73172a4abf6b0a1
nurmormor <nurmormor@gmail.com>
2025-07-17 16:38:23 +0200

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./components/**/*.html"], // Asegúrate de que Tailwind procese estos archivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
